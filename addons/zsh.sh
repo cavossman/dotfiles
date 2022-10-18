213523fa594b36b545a8cf495f054979ff9fa553
Collin Vossman <cavossman@gmail.com>
2022-10-18 09:20:01 -0500

    activate omz theme/plugins

--- addons/zsh.sh
+++ addons/zsh.sh
@@ -1,12 +1,14 @@
 # Install and configure ohmyzsh
 
-# TODO: check if ohmyzsh is already installed/configured
+# TODO: check if ohmyzsh and plugins are already installed/configured
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 # Install custom zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Set up plugins/theme in zsh shell
+zsh -ic "omz plugin enable zsh-autosuggestions"
+zsh -ic "omz theme set eastwood"
+
 # Set aliases
 cp ${SCRIPT_DIR}/aliases.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-
-# TODO: write sed statement to activate plugins in ~/.zshrc
--- bootstrap.sh
+++ bootstrap.sh
@@ -36,4 +36,4 @@ source ${SCRIPT_DIR}/addons/packages.sh
 # STEP 4: Copy Scripts to PATH
 
 chmod +x ./scripts/provision
-cp ./script/provision /usr/local/bin
+cp ./scripts/provision /usr/local/bin
